8861f27f5d87b6ce98b044cdd919e138718b0a4c
Arne Schröder <Arne.Schroeder@gmail.com>
2014-03-06 17:02:44 +0100

    Knoten werden vor Kanten aufgesammelt, Zuordnung: Spieler - Formen korrigiert

--- src/parisolve/io/ArenaManager.java
+++ src/parisolve/io/ArenaManager.java
@@ -22,12 +22,14 @@ public class ArenaManager {
 	public static Arena loadArena(String fileName) throws IOException {
 		List<String> lines = Files.readAllLines(Paths.get(fileName), Charset.defaultCharset());
 		LinkedArena arena = new LinkedArena();
+		//TODO encapsulate this and parse properly
 		for (String line : lines) {
-			//TODO encapsulate this and parse properly
 			Matcher vertexMatcher = VERTEX_PATTERN.matcher(line);
 			if (vertexMatcher.find()) {
-				arena.addVertex(vertexMatcher.group(1), Integer.parseInt(vertexMatcher.group(3)), "box".equals(vertexMatcher.group(2)) ? 0 : 1);
+				arena.addVertex(vertexMatcher.group(1), Integer.parseInt(vertexMatcher.group(3)), "box".equals(vertexMatcher.group(2)) ? 1 : 0);
 			}
+		}
+		for (String line : lines) {
 			Matcher edgeMatcher = EDGE_PATTERN.matcher(line);
 			if (edgeMatcher.find()) {
 				arena.addEdge(edgeMatcher.group(1), edgeMatcher.group(2));
